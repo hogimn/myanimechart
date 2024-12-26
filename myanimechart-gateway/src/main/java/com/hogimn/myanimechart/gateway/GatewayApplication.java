8d6c87291e52ec448b26d8d034d22054ce35e3c6
hogimn <hogimn@gmail.com>
2024-12-27 00:45:44 +0900

    Add cors config for gateway

--- myanimechart-gateway/src/main/java/com/hogimn/myanimechart/gateway/GatewayApplication.java
+++ myanimechart-gateway/src/main/java/com/hogimn/myanimechart/gateway/GatewayApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
-        "com.hogimn.myanimechart.gateway"
+        "com.hogimn.myanimechart.gateway",
+        "com.hogimn.myanimechart.common.cors"
 })
 public class GatewayApplication {
     public static void main(String[] args) {
