df1c0f1d8f9878a99eede4788b6d6281ccd0bcf2
hogimn <hogimn@gmail.com>
2024-12-27 00:48:58 +0900

    Revert "Add scanBasePackage cors in gateway"
    This reverts commit a0cd67c59f5813bbbec31935150479fd28623c97.

--- myanimechart-gateway/src/main/java/com/hogimn/myanimechart/gateway/GatewayApplication.java
+++ myanimechart-gateway/src/main/java/com/hogimn/myanimechart/gateway/GatewayApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
-        "com.hogimn.myanimechart.gateway",
-        "com.hogimn.myanimechart.common.cors"
+        "com.hogimn.myanimechart.gateway"
 })
 public class GatewayApplication {
     public static void main(String[] args) {
