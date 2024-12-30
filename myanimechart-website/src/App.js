020412201074c6480156734932978a09074db110
hogimn <hogimn@gmail.com>
2024-12-30 15:52:20 +0900

    Deploy to github page

--- myanimechart-website/src/App.js
+++ myanimechart-website/src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
 import AnimeStat from "./components/pages/animestat/AnimeStat";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path={"/"} element={<Navigate to={"/animeStat"} replace />}/>
                 <Route path={"/animeStat"} element={<AnimeStat/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
