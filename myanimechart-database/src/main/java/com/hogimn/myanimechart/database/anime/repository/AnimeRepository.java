2d0e6bed8ba865ef3b915af0f3279ce85a95697a
hogimn <hogimn@gmail.com>
2025-02-10 00:35:58 +0900

    Do not use forceCollect for poll

--- myanimechart-database/src/main/java/com/hogimn/myanimechart/database/anime/repository/AnimeRepository.java
+++ myanimechart-database/src/main/java/com/hogimn/myanimechart/database/anime/repository/AnimeRepository.java
@@ -28,13 +28,12 @@ public interface AnimeRepository extends JpaRepository<AnimeEntity, Long> {
                                                                 String currentlyAiring, String finishedAiring);
 
     @Query("SELECT a FROM AnimeEntity a " +
-            "WHERE (a.airStatus = :currentlyAiring OR " +
+            "WHERE a.airStatus = :currentlyAiring OR " +
             "(a.airStatus = :finishedAiring AND " +
             "(EXTRACT(MONTH FROM a.endDate) = EXTRACT(MONTH FROM CURRENT_TIMESTAMP) " +
             "OR EXTRACT(MONTH FROM CURRENT_TIMESTAMP) = CASE " +
             "WHEN EXTRACT(MONTH FROM a.endDate) = 12 THEN 1 " +
-            "ELSE EXTRACT(MONTH FROM a.endDate) + 1 END))) " +
-            "OR a.forceCollect = 'Y'")
+            "ELSE EXTRACT(MONTH FROM a.endDate) + 1 END))")
     List<AnimeEntity> findAnimeEntitiesForPollCollection(String currentlyAiring, String finishedAiring);
 
     List<AnimeEntity> findAllByTitleContaining(String title);
