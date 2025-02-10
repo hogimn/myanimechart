dc172d9377e5234ae59820aaa33f79fb783f3e8a
hogimn <hogimn@gmail.com>
2025-02-10 23:11:27 +0900

    Style summary modal

--- myanimechart-website/src/components/common/basic/CommonModal.js
+++ myanimechart-website/src/components/common/basic/CommonModal.js
@@ -1,8 +1,18 @@
 import { Modal } from "antd";
 import React from "react";
+import styled from "styled-components";
+
+const StyledModal = styled(Modal)`
+  .ant-modal-content {
+    background-color: rgb(32, 34, 43) !important;
+  }
+  .ant-modal-title {
+    background-color: rgb(32, 34, 43) !important;
+  }
+`;
 
 const CommonModal = ({ children, ...restProps }) => {
-  return <Modal {...restProps}>{children}</Modal>;
+  return <StyledModal {...restProps}>{children}</StyledModal>;
 };
 
 export default CommonModal;
