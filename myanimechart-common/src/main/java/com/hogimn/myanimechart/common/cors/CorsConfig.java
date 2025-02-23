3648d8f2f7f498aa728ea40ab44e04b6625b97f7
hogimn <hogimn@gmail.com>
2025-02-23 23:11:44 +0900

    Add cors origin

--- myanimechart-common/src/main/java/com/hogimn/myanimechart/common/cors/CorsConfig.java
+++ myanimechart-common/src/main/java/com/hogimn/myanimechart/common/cors/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3001", "https://myanimechart.com")
+                .allowedOrigins("http://localhost:3001", "https://myanimechart.com", "https://api.myanimechart.com")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true);
