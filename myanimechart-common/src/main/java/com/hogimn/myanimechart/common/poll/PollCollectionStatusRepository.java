e0beb58bf11788cadff4ba6b88d2be8c6b441e38
hogimn <hogimn@gmail.com>
2025-03-14 17:14:15 +0900

    Remove unused method

--- myanimechart-common/src/main/java/com/hogimn/myanimechart/common/poll/PollCollectionStatusRepository.java
+++ myanimechart-common/src/main/java/com/hogimn/myanimechart/common/poll/PollCollectionStatusRepository.java
@@ -24,18 +24,6 @@ public interface PollCollectionStatusRepository extends JpaRepository<PollCollec
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<PollCollectionStatusEntity> findByStatusWithLock(CollectionStatus collectionStatus);
 
-    @Query("SELECT b FROM PollCollectionStatusEntity b " +
-            "WHERE b.animeId IN (" +
-            "SELECT a.id FROM AnimeEntity a " +
-            "WHERE NOT (" +
-            "a.airStatus = :currentlyAiring OR " +
-            "(a.airStatus = :finishedAiring AND " +
-            "(EXTRACT(MONTH FROM a.endDate) = EXTRACT(MONTH FROM CURRENT_TIMESTAMP) " +
-            "OR EXTRACT(MONTH FROM CURRENT_TIMESTAMP) = CASE " +
-            "WHEN EXTRACT(MONTH FROM a.endDate) = 12 THEN 1 " +
-            "ELSE EXTRACT(MONTH FROM a.endDate) + 1 END))))")
-    List<PollCollectionStatusEntity> findUnusedPollCollectionStatus(String currentlyAiring, String finishedAiring);
-
     @Query("""
             SELECT a
             FROM PollCollectionStatusEntity a
