2051975b6cc3f8730371fc12ceda6ac4ce5ca251
hogimn <hogimn@gmail.com>
2025-03-30 12:32:22 +0900

    Add anime keyword mapping

---
+++ myanimechart-common/src/main/resources/flyway-common/V38__poll.sql
@@ -0,0 +1,8 @@
+INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
+VALUES (53111, 'Dungeon ni Deai wo Motomeru no wa Machigatteiru Darou ka IV: Fuka Shou - Yakusai-hen');
+
+INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
+VALUES (49709, 'Fumetsu no Anata e 2nd Season');
+
+INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
+VALUES (49709, 'Shin Tennis no Ouji-sama: U-17 World Cup');
