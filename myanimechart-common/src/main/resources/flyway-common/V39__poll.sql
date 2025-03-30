e69743237ad5e2f45f770f334e85c82147d039d6
hogimn <hogimn@gmail.com>
2025-03-30 13:00:47 +0900

    Remove anime master data that are not tv series

---
+++ myanimechart-common/src/main/resources/flyway-common/V39__poll.sql
@@ -0,0 +1,17 @@
+DELETE FROM anime_keyword_mapping
+WHERE anime_id = 12345;
+
+DELETE FROM anime_keyword_mapping
+WHERE anime_id = 59868;
+
+DELETE FROM anime_episode_topic_mapping
+WHERE anime_id = 58797;
+
+DELETE FROM anime_episode_topic_mapping
+WHERE anime_id = 60048;
+
+DELETE FROM anime_episode_topic_mapping
+WHERE anime_id = 60338;
+
+DELETE FROM anime_episode_topic_mapping
+WHERE anime_id = 60746;
