9ae8d636343db18ce1ee6d17914377fc0d50409d
hogimn <hogimn@gmail.com>
2025-04-12 16:35:20 +0900

    Ignore duplicate row

--- myanimechart-common/src/main/resources/flyway-common/V44__poll.sql
+++ myanimechart-common/src/main/resources/flyway-common/V44__poll.sql
@@ -1,41 +1,16 @@
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (40776, 'Haikyuu!!: To the Top 2nd Season');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (38889, 'Kono Oto Tomare! 2nd Season');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (39811, 'Fairy Gone 2nd Season');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (38256, 'Magia Record: Mahou Shoujo Madoka☆Magica Gaiden (TV)');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (40716, 'Kakushigoto (TV)');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (40504, 'Major 2nd (TV) 2nd Season');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (40506, 'Shadowverse (TV)');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (40496, 'Maou Gakuin no Futekigousha: Shijou Saikyou no Maou no Shiso, Tensei shite Shison-tachi no Gakkou e');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (42941, 'Uma Musume: Pretty Derby (TV) Season 2');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (41491, 'Nanatsu no Taizai: Fundo no Shinpan');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (40961, 'Hortensia Saga (TV)');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (45207, 'Okashi na Sabaku no Suna to Manu');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (32455, 'Gekidol');
-
-INSERT INTO anime_keyword_mapping (anime_id, search_keyword)
-VALUES (43325, 'Yuukoku no Moriarty 2nd Season');
+INSERT IGNORE INTO anime_keyword_mapping (anime_id, search_keyword)
+VALUES
+(40776, 'Haikyuu!!: To the Top 2nd Season'),
+(38889, 'Kono Oto Tomare! 2nd Season'),
+(39811, 'Fairy Gone 2nd Season'),
+(38256, 'Magia Record: Mahou Shoujo Madoka☆Magica Gaiden (TV)'),
+(40716, 'Kakushigoto (TV)'),
+(40504, 'Major 2nd (TV) 2nd Season'),
+(40506, 'Shadowverse (TV)'),
+(40496, 'Maou Gakuin no Futekigousha: Shijou Saikyou no Maou no Shiso, Tensei shite Shison-tachi no Gakkou e'),
+(42941, 'Uma Musume: Pretty Derby (TV) Season 2'),
+(41491, 'Nanatsu no Taizai: Fundo no Shinpan'),
+(40961, 'Hortensia Saga (TV)'),
+(45207, 'Okashi na Sabaku no Suna to Manu'),
+(32455, 'Gekidol'),
+(43325, 'Yuukoku no Moriarty 2nd Season');
