e128ddd04956f2cbaeb38dcf5aef831ba9211d38
hogimn <hogimn@gmail.com>
2025-04-13 21:32:57 +0900

    Sleep for MAL 2sec

--- myanimechart-common/src/main/java/com/hogimn/myanimechart/common/util/SleepUtil.java
+++ myanimechart-common/src/main/java/com/hogimn/myanimechart/common/util/SleepUtil.java
@@ -11,7 +11,7 @@ public class SleepUtil {
 
     public static void sleepForMAL() {
         try {
-            Thread.sleep(1000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
