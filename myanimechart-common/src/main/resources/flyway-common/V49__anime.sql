e46a6e746454fda26e47192827d58e30a6d1148f
hogimn <hogimn@gmail.com>
2025-06-08 22:26:42 +0900

    Add anime mapping

---
+++ myanimechart-common/src/main/resources/flyway-common/V49__anime.sql
@@ -0,0 +1,3 @@
+ALTER TABLE myanimechart.anime MODIFY COLUMN start_date DATETIME;
+ALTER TABLE myanimechart.anime MODIFY COLUMN created_at DATETIME;
+ALTER TABLE myanimechart.anime MODIFY COLUMN updated_at DATETIME;
