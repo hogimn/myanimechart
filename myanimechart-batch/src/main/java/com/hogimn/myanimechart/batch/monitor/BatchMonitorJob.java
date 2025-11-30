c97920696e41207b79383b7cd78668266c8279c1
hogimn <hogimn@gmail.com>
2025-11-30 16:19:35 +0900

    Revert "Delete batch monitor job"
    This reverts commit 1fb1de8d9f1e3c46355dbdaa3f5de3f5c232ed5f.

---
+++ myanimechart-batch/src/main/java/com/hogimn/myanimechart/batch/monitor/BatchMonitorJob.java
@@ -0,0 +1,40 @@
+package com.hogimn.myanimechart.batch.monitor;
+
+import com.hogimn.myanimechart.common.batch.BatchDto;
+import com.hogimn.myanimechart.common.batch.BatchService;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class BatchMonitorJob {
+    private final BatchService batchService;
+    private final BatchMonitorService batchMonitorService;
+    private final ThreadPoolTaskScheduler threadPoolTaskScheduler;
+
+    public BatchMonitorJob(
+            BatchService batchService,
+            BatchMonitorService batchMonitorService,
+            ThreadPoolTaskScheduler threadPoolTaskScheduler
+    ) {
+        this.batchService = batchService;
+        this.batchMonitorService = batchMonitorService;
+        this.threadPoolTaskScheduler = threadPoolTaskScheduler;
+    }
+
+    @PostConstruct
+    public void scheduleBatchMonitorTask() {
+        BatchDto batchDto = batchService.findBatchDtoByName(this.getClass().getSimpleName());
+
+        threadPoolTaskScheduler.schedule(
+                () -> checkBatchNotExecuted(batchDto.getName()),
+                new CronTrigger(batchDto.getCron()));
+    }
+
+    public void checkBatchNotExecuted(String batchJobName) {
+        batchMonitorService.checkBatchNotExecuted(batchJobName);
+    }
+}
---
+++ myanimechart-batch/src/main/java/com/hogimn/myanimechart/batch/monitor/BatchMonitorService.java
@@ -0,0 +1,50 @@
+package com.hogimn.myanimechart.batch.monitor;
+
+import com.hogimn.myanimechart.common.alarm.AlarmService;
+import com.hogimn.myanimechart.common.batch.BatchDto;
+import com.hogimn.myanimechart.common.batch.BatchHistoryService;
+import com.hogimn.myanimechart.common.batch.BatchService;
+import com.hogimn.myanimechart.common.batch.SaveBatchHistory;
+import com.hogimn.myanimechart.common.util.CronUtil;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class BatchMonitorService {
+    private final BatchService batchService;
+    private final BatchHistoryService batchHistoryService;
+    private final List<AlarmService> alarmServices;
+
+    public BatchMonitorService(BatchService batchService,
+                               BatchHistoryService batchHistoryService,
+                               List<AlarmService> alarmServices) {
+        this.batchService = batchService;
+        this.batchHistoryService = batchHistoryService;
+        this.alarmServices = alarmServices;
+    }
+
+    @SaveBatchHistory("#batchJobName")
+    @SchedulerLock(name = "checkBatchNotExecuted")
+    public void checkBatchNotExecuted(String batchJobName) {
+        List<BatchDto> batchDtos = batchService.findAllBatchDtos();
+        for (BatchDto batchDto : batchDtos) {
+            String cron = batchDto.getCron();
+            long period = CronUtil.getPeriodAsSeconds(cron);
+            if (!batchHistoryService.checkBatchExecutedWithinPeriod(batchDto.getName(), period)) {
+                String errorMessage = String.format("batch not executed within period: %s %s.", batchDto.getName(), period);
+                log.error(errorMessage);
+                alarmServices.forEach(alarmService -> {
+                    if (alarmService.isSupported()) {
+                        alarmService.send(errorMessage);
+                    }
+                });
+            } else {
+                log.info("batch executed within period: {} {}.", batchDto.getName(), period);
+            }
+        }
+    }
+}
